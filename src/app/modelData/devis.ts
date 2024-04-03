e2e9addc29105f1784c357b7e1f8cfc976aff75d
fawzy20 <fawzy-elsam@outlook.fr>
2024-04-03 17:25:03 +0200

    ajout du model data pour le devis

---
+++ src/app/modelData/devis.ts
@@ -0,0 +1,20 @@
+export class Devis{
+    societe!:string;
+    adresseSociete!:string;
+    codePostalSociete!:string;
+    villeSociete!:string;
+    siretSociete!:string;
+    tvaSociete!:string;
+    telSociete!:string;
+    nomClient!:string;
+    adresseClient!:string;
+    codePostalClient!:string;
+    villeClient!:string;
+    siretClient!:string;
+    telClient!:string;
+    dateDevis!:string;
+    infoDevis!: any[];
+    totalHt!:string;
+    tva!:string;
+    totalTtc!:string;
+}
